f28782a56e66cbc22242ff0115621a31ea0e4bac
dtimaspk <dtimaspk@gmail.com>
2017-07-13 11:58:42 -0100

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class Game extends App{
+	public void setup(){
+		setFps(60);
+		setAltura(600);
+		setLargura(1000);
+	}
+	public void draw(Graphics g){
+	}
+	public void update(){
+	}
+	public void checkInput(){ 
+	}
+}
